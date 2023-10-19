cfb1bab61c25893d2d2e6d420a9d9c68ec51e7b7
meliodas0n <mrunalnarayana@gmail.com>
2023-10-19 23:50:18 +0530

    indentation to 2 and removed vscode from gitignore

--- backend/main.py
+++ backend/main.py
@@ -7,18 +7,18 @@ app = FastAPI()
 
 
 class App:
-    async def run() -> None:
-        config = uvicorn.Config(
-            app,
-            port=8000,
-            use_colors=True,
-            reload=True,
-            reload_delay=1,
-            log_level="info",
-        )
-        server = uvicorn.Server(config)
-        await server.serve()
+  async def run() -> None:
+      config = uvicorn.Config(
+          app,
+          port=8000,
+          use_colors=True,
+          reload=True,
+          reload_delay=1,
+          log_level="info",
+      )
+      server = uvicorn.Server(config)
+      await server.serve()
 
 
 if __name__ == "__main__":
-    asyncio.run(App.run())
+  asyncio.run(App.run())
