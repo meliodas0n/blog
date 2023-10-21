410f39e4f3cfb14cee71c270cac584b6e618336a
meliodas0n <mrunalnarayana@gmail.com>
2023-10-21 18:09:23 +0530

    pages and starting routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,47 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+]);
+
 function App() {
   return (
     <>
-      <div className="main-container">
-        <h1 className="main-heading">My Life for Fun</h1>
+      <div className="text-9xl">
+        <RouterProvider router={router} />
       </div>
     </>
   );
